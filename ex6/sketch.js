1ea55e30f374ea967d0160f84ecea3b9e4bed824
Ricki@mac <rickixie@users.noreply.github.com>
2016-02-14 21:37:39 -0800

    Revert "Merge branch 'master' into bonny"
    This reverts commit 43dc9540597da56e63d51b26d534c64c76c72002, reversing
    changes made to f1b956a4853eead0b22ccd3cfc462be1af27fc81.

--- ex6/sketch.js
+++ ex6/sketch.js
@@ -1,26 +1,28 @@
+// Define any global variables here
+// (BUT don't call any p5 methods here;
+//  call them in the functions below!)
+// 
+// var SOCKET_URL = 'wss://fierce-plains-17880.herokuapp.com/';
+// var TEAM_NAME  = '';
+// var socket; 
+// 
+// function setup() {
+//   createCanvas(windowWidth, windowHeight); // Use the full browser window
+//   socket = io(SOCKET_URL + TEAM_NAME); // Open a socket connection to the server.
+//   Additional setup goes here. E.g., registering socket.on handlers. 
+// }
+// 
+// function draw() {
+//   Put your drawing code here
+// }
+// 
+// Define any additional helper functions here
+
+
 var ballArray = [];//create a array to store the ball
-var clouds = [];//create a array for cloud
 var newHue = 100;//preset the hue to a fix value to avoid null exception
 var pitch1 = [];//create a array to store the sound file for pitch 1
 //initiate sound files 
-<<<<<<< HEAD
-// var do1;
-// var re1;
-// var mi1;
-// var fa1;
-// var sol1;
-// var la1;
-// var til1;
-var pitchSetIndex = 0;
-// var backgroundColor = 220;
-var counter = 0;
-
-var SOCKET_URL = 'wss://fierce-plains-17880.herokuapp.com/';
-var TEAM_NAME  = 'dreamcatcher';
-var socket; 
-//shapes
-var lt = [];
-=======
 var do1;
 var re1;
 var mi1;
@@ -29,86 +31,27 @@ var sol1;
 var la1;
 var til1;
 var pitchSet = 0;
-// var backgroundColor = 220;
+var backgroundColor = 220;
 var counter = 0;
-// var bgcolor = [];
-// var midnight;
-// var plum;
-// var grey;
-// var forest;
-// var lightgrey;
-// var peach;
-// var lilac; 
+var bgcolor = [];
+var midnight;
+var plum;
+var grey;
+var forest;
+var lightgrey;
+var peach;
+var lilac; 
 var SOCKET_URL = 'wss://fierce-plains-17880.herokuapp.com/';
 var TEAM_NAME  = 'dreamcatcher';
 var socket; 
 
-//shapes
-
-var lt = [];
-
-
-
->>>>>>> ricki
 // var soundKey = 0; causes one tone to be played 
+
 /* preload the sound to use before the program run, 
 * it is cused to handle asynchronous loading of external files. 
 * If a preload function is defined, setup() will wait until 
 * any load calls within have finished. 
 **/
-<<<<<<< HEAD
-function preload() {
-  
-  //load ball sounds
-  var ballpitchset = [];
-  pitch1.push(ballpitchset);
-  //Sound 
-  do1 = loadSound('music/01do.mov');
-  ballpitchset.push(do1);//position 0
-  re1 = loadSound('music/01re.mov');
-  ballpitchset.push(re1);//position 1
-  mi1 = loadSound('music/01mi.mov');
-  ballpitchset.push(mi1);//position 2
-  fa1 = loadSound('music/01fa.mov');
-  ballpitchset.push(fa1);//position 3
-  sol1 = loadSound('music/01sol.mov');
-  ballpitchset.push(sol1);//position 4
-  la1 = loadSound('music/01la.mov');
-  ballpitchset.push(la1);//position 5
-  ti1 = loadSound('music/01ti.mov');
-  ballpitchset.push(ti1);//position 6
-  
-  //load cloud sounds
-  var cloudpitchset = [];
-  pitch1.push(cloudpitchset);
-  c1 = loadSound ('music/C1.mov'); //(c=cloud)
-  cloudpitchset.push(c1);
-  c2 = loadSound ('music/C2.mov');
-   cloudpitchset.push(c2);
-  c3 = loadSound ('music/C3.mov');
-   cloudpitchset.push(c3);
-  c4 = loadSound ('music/C4.mov');
-   cloudpitchset.push(c4);
-  c5 = loadSound ('music/C5.mov');
-   cloudpitchset.push(c5);
-   
-  var lightningpitchset = [];
-  pitch1.push(lightningpitchset); 
-  l1 = loadSound ('music/L1.mov'); //(l=lighting)
-  lightningpitchset.push(l1);
-  l2 = loadSound ('music/L2.mov');
-  lightningpitchset.push(l2);
-  l3 = loadSound ('music/L3.mov');
-  lightningpitchset.push(l3);
-  
-  
-  // var lt1= loadImage("img/lightning-02.svg");
-  // lt.push(lt1);
-  // var lt2= loadImage("img/lightning-03.svg");
-  // lt.push(lt2);
-  // var lt3= loadImage("img/lightning-04.svg");
-  // lt.push(lt3);
-=======
 
 function preload() {
   var pitchset = [];
@@ -134,34 +77,23 @@ function preload() {
   // pitchset.push(loadSound('music/'));
   
   //Background Color
-  // midnight = color('hsl(255,19%, 25%)');
-  // plum = color('hsl(349, 24%, 35%)');
-  // grey = color('hsl(219, 0%, 19%)');
-  // forest = color('hsl(108, 34%, 37%)');
-  // lightgrey = color('hsl(113, 0%, 73%)');
-  // peach = color ('hsl(22, 100%, 70%)');
-  // lilac = color('hsl(256,37%, 77%)');
+  midnight = color('hsl(255,19%, 25%)');
+  plum = color('hsl(349, 24%, 35%)');
+  grey = color('hsl(219, 0%, 19%)');
+  forest = color('hsl(108, 34%, 37%)');
+  lightgrey = color('hsl(113, 0%, 73%)');
+  peach = color ('hsl(22, 100%, 70%)');
+  lilac = color('hsl(256,37%, 77%)');
 
-  // console.log(midnight); //first color presented
-  // bgcolor.push(plum);
-  // bgcolor.push(grey);
-  // bgcolor.push(forest);
-  // bgcolor.push(lightgrey);
-  // bgcolor.push(peach);
-  // bgcolor.push(lilac);  
-  
-  
-  var lt1= loadImage("img/lightning-02.svg");
-  lt.push(lt1);
-  var lt2= loadImage("img/lightning-03.svg");
-  lt.push(lt2);
-  var lt3= loadImage("img/lightning-04.svg");
-  lt.push(lt3);
->>>>>>> ricki
-  
-  // var lt1= loadImage("img/lightnight-02.svg");
-  
+  console.log(midnight); //first color presented
+  bgcolor.push(plum);
+  bgcolor.push(grey);
+  bgcolor.push(forest);
+  bgcolor.push(lightgrey);
+  bgcolor.push(peach);
+  bgcolor.push(lilac);  
 }
+
 /*
 * The setup() function is called once when the program starts. 
 * It's used to define initial environment properties such as 
@@ -169,54 +101,23 @@ function preload() {
 * images and fonts as the program starts. 
 * http://p5js.org/reference/#/p5/setup
 */
-<<<<<<< HEAD
-=======
 
->>>>>>> ricki
 function setup(){
   canvas = createCanvas(windowWidth, windowHeight);
   frameRate(30);
-  // console.log(lt);
   socket = io(SOCKET_URL + TEAM_NAME); // Open a socket connection to the server.
   // Additional setup goes here. E.g., registering socket.on handlers.  
-  socket.on('createBall', createBallLocal);
-  socket.on('createCloud', createCloudLocal);
-  // socket.on('lightning', mouseClicked());
-  
+  socket.on('ball', createBallLocal);
+  // socket.on('keypressed',keypressed);
 }
-<<<<<<< HEAD
-function draw(){
-  // background(backgroundColor);
-  background('hsl(0,0%, 18%)');
-  for (var i=0; i<ballArray.length; i++){
-    ballArray[i].bounce3();
-    ballArray[i].display();
-  }
-  
-  //draw clouds
-  for (var j=0; j<clouds.length; j++){
-    clouds[j].display();
-  }
-  
-=======
 
 function draw(){
-  // background(backgroundColor);
-  background('hsl(0,0%, 18%)');
+  background(backgroundColor);
 
   for (var i=0; i<ballArray.length; i++){
     ballArray[i].bounce3();
     ballArray[i].display();
   }
-  
-
-  //draw clouds
-  for (var j=0; j<clouds.length; j++){
-    // clouds[j].flash();
-    clouds[j].display(); 
-  }
-  
->>>>>>> ricki
 }
 
 function windowResized() {
@@ -234,12 +135,6 @@ function windowResized() {
       element.y = element.diameter;
     }
   }); 
-
-  clouds.forEach(function(element, index, array){
-    //make the cloud shows up porportionlly to the window width;
-    
-
-  });
 }
 
 //Create a number of ball based on the keypress value received from keyPressed().
@@ -250,11 +145,7 @@ function createBall(pitch){ //used to be (numball)
 
 function createBallLocal(pitch) {
   // for (var i=0; i<numBall; i++){
-<<<<<<< HEAD
-    var newBall = new Ball(mouseX, mouseY, pitch+1, pitch1[pitchSetIndex][pitch]);//create a new ball
-=======
     var newBall = new Ball(mouseX, mouseY, pitch+1, pitch1[pitchSet][pitch]);//create a new ball
->>>>>>> ricki
     ballArray.push(newBall);//put into the array
   // }
   
@@ -267,86 +158,17 @@ function createBallLocal(pitch) {
 }
 
 function createBallRemote(pitch) {
-<<<<<<< HEAD
   socket.emit('ball', pitch);
 }
-=======
-  socket.emit('createBall', pitch);
-}
 
-
-//Create cloud based on mouse click.
-function createCloud(){ 
-  createCloudLocal();
-  createCloudRemote();
-}
-
->>>>>>> ricki
-
-//Create cloud based on mouse click.
-function createCloud(){ 
-  createCloudLocal();
-  createCloudRemote();
-}
-
-function createCloudLocal(){
-  var newCloud = new cloud (mouseX, mouseY);
-  clouds.push(newCloud);
-<<<<<<< HEAD
-=======
-
->>>>>>> ricki
-   if (clouds.length > 4) {
-    var diff = clouds.length - 4;
-    for (var j=0; j<diff; j++){
-      clouds.shift();
-    }
-  }
-}
-
-function createCloudRemote(){
-<<<<<<< HEAD
-  socket.emit('cloud', mouseX, mouseY);
-}
-
-=======
-  socket.emit('createCloud', mouseX, mouseY);
-}
-
-
->>>>>>> ricki
 function mouseClicked(){
-  createCloud();
+    if(counter === bgcolor.length-1){
+      counter = 0;
+    }
+    backgroundColor = bgcolor[counter];
+    counter++;
 }
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> ricki
-// function mouseClicked(){
-//     // // if(counter === bgcolor.length-1){
-//     // //   counter = 0;
-//     // // }
-//     // // backgroundColor = bgcolor[counter];
-//     // // counter++;
-    
-//     if(counter === (lt.length)-1){
-//       counter = 0;
-//     }
-//     image(lt[counter], 0, 0);
-//     socket.emit("lightning", counter);
-//     console.log(counter);
-//     counter++;
-// }
-<<<<<<< HEAD
-=======
-
-
-
-
->>>>>>> ricki
 //Handle keyPressEvent
 function keyPressed(){
   //https://css-tricks.com/snippets/javascript/javascript-keycodes/
@@ -372,13 +194,13 @@ function keyPressed(){
      case 52: {//Number: 4
        createBall(3);
         // playSound(3);
-        // soundKey = 4;
+          // soundKey = 4;
        break;
      }
      case 53: {//Number: 5
        createBall(4);
         // playSound(4);
-        // soundKey = 5;
+          // soundKey = 5;
        break;
      }
      case 54: {//Number: 6
@@ -398,56 +220,32 @@ function keyPressed(){
         while(ballArray.length){
           ballArray.pop();
         }
-        //clear the clouds
-        while(clouds.length){
-          clouds.pop();
-        }
       
         getHue();//change the color
         //TODO: stop the music
         break;
      }
-    case 32: {//Space
-<<<<<<< HEAD
-      pitchSetIndex++;
-      if (pitchSetIndex == pitch1.length) {
-        pitchSetIndex = 0;
-      }
-      break;
-    }
-    // case 18: {//Alt
-      
-    //   createCloud(mouseX, mouseY);
-    //   break;
-    // }
-=======
+    case 32: {
       pitchSet++;
       if (pitchSet == pitch1.length) {
         pitchSet = 0;
       }
       break;
     }
-    case 18: {//Alt
-    for(var i=0; i<clouds.length; i++){
-      clouds[i].flash();
-      clouds[i].display();
-    } 
-
-      // createCloud(mouseX, mouseY);
-      break;
-    }
->>>>>>> ricki
   }
 }
+
 //A Helper function to play sound
 function playSound(pitch){
   pitch.setVolume(0.1);
   pitch.play();//http://p5js.org/reference/#/p5.SoundFile/play
 }
+
 //A Helper function to get a hue value to create mono color 
 function getHue(){
   newHue = floor(random(360));
 }
+
 //A Helper function to create a color with the same hue value
 function changeAllColor(){
     var h = newHue;//get the hue value
@@ -465,25 +263,25 @@ function changeAllColor(keyColor){
     var s = 90;
   switch(keyColor){
     case 1: {//key pressed 1;
-       h = 190;
+       h = 8; //red
       break;
-     }case 2: {//key pressed 2;
-       h = 200; 
+     }case 2: {//key pressed 1;
+       h = 23; //orange
        break;
-     }case 3: {//key pressed 3;
-       h = 210; 
+     }case 3: {//key pressed 1;
+       h = 48; //yellow
        break;
-     }case 4: {//key pressed 4;
-       h = 220; 
+     }case 4: {//key pressed 1;
+       h = 81; //green
        break;
-     }case 5: {//key pressed 5;
-       h = 230; 
+     }case 5: {//key pressed 1;
+       h = 164; //cyan
        break;
-     }case 6: {//key pressed 6;
-       h = 240; 
+     }case 6: {//key pressed 1;
+       h = 196; //blue
        break;
-     }case 7: {//key pressed 7;
-       h = 250;
+     }case 7: {//key pressed 1;
+       h = 227;//dark-blue
        break;
      }
     
@@ -497,18 +295,15 @@ function changeAllColor(keyColor){
     var c = color('hsla('+h+','+s+'%,'+l+'%,'+a+')');
     return c;
 }
+
 //The Ball Constructor
 //Bounce ball with gravity: http://www.openprocessing.org/sketch/47766
 //Bounce ball and change color when hits the edges: http://www.openprocessing.org/sketch/110555
 function Ball(x, y, number, pitch) {
-<<<<<<< HEAD
-=======
-
->>>>>>> ricki
+  //set random starting point
   // var diameter = random(50, 100);
   playSound(pitch); //new plays sound on ball creation 
-  this.diameter = 10;
-  // this.diameter = random(1,10); //old 10, 100
+  this.diameter = random(10,100);
   if (x > width - this.diameter || x < this.diameter) {
     x = random(this.diameter, width - this.diameter);
   }
@@ -521,17 +316,18 @@ function Ball(x, y, number, pitch) {
   
   //set a random diameter size
   var radius = this.diameter/2;
-  var vx = 10;
-  var vy = 10;
-  // var vx = random(5,10);
-  // var vy = random(5,10);
+  var vx = random(5,20);
+  var vy = random(5,20);
   
   //define the color for the ball
   this.color = changeAllColor(number);
-<<<<<<< HEAD
-=======
 
->>>>>>> ricki
+  this.speed = 0.7;
+
+  this.move = function() {
+    this.x += random(-this.speed, this.speed);
+    this.y += random(-this.speed, this.speed);
+  };
     
   this.display = function() {
     fill(this.color);//only fill the color when you create the ball
@@ -539,10 +335,16 @@ function Ball(x, y, number, pitch) {
     ellipse(this.x, this.y, this.diameter, this.diameter);
    
   };
-<<<<<<< HEAD
-=======
 
->>>>>>> ricki
+  //distance
+  var xdistance = this.diameter/2;
+  var ydistance = this.diameter/2;
+  //speed
+  var speedY = 20;
+  var speedX = 20;
+  //direction
+  var ydirection = 1;
+  var xdirection = 1;
   
   this.bounce3 = function(){//http://www.openprocessing.org/sketch/218262
     if (((this.x + vx) > width - radius) || ((this.x) < radius)) {
@@ -561,84 +363,17 @@ function Ball(x, y, number, pitch) {
   }
   //TODO: Collision
   //http://p5play.molleindustria.org/examples/index.html?fileName=collisions4.js
-<<<<<<< HEAD
+  
+  this.bounce = function(){
+    if ((this.x+speedX) > width-(this.diameter)/2 || (this.x+speedX) < this.diameter/2) {
+      speedX = -speedX;
+    }
+  
+    if ((this.y+speedY) > height-(this.diameter)/2 ||(this.y+speedY)<(this.diameter)/2) {
+      speedY= -speedY;
+    }
+     
+    this.x = this.x + speedX;
+    this.y = this.y + speedY;
+  }
 }
-function cloud(x,y){
-  // var intX = this.x/2;
-  this.x = x;
-  this.y = y;
-  // var h = height/2;
-=======
-
-}
-
-function cloud(x,y){
- 
-  this.x = x;
-  this.y = y;
->>>>>>> ricki
- 
- 
- this.display = function(){
-  //simple cloud x, h is starting position
-  fill(200, 200, 255);
-  noStroke();
-  ellipse(this.x + 50, this.y, 50, 50);
-  ellipse(this.x + 20, this.y, 50, 50);
-  ellipse(this.x + 70, -20 + this.y, 50, 50);
-  ellipse(this.x + 35, -30 + this.y, 50, 50);
-  ellipse(this.x, -20 + this.y, 50, 50);
-<<<<<<< HEAD
- };
-}
-=======
-
-  //midpoint --> (this.x+35, this.y-15);
- };
-
- this.flash = function (){
-  var newLightning = new lightning(this.x+35, this.y-15);
-  newLightning.display();
- }
-}
-
-
-function lightning(x,y){
-
-  this.x = x;
-  this.y = y;
- 
- //  var startX;
- // var startY;
- // startX = width/2;
- // startY = height/2;
- var a = this.x + random(20,50);
- var b = this.y + random(60,125);
- var c = random(-50,-70);
- var d = random(40,80);
- var e = random(20,60);
- var f = random(20,60);
- var g = random(-20, -50);
- var h = random (20, 60);
- 
-   // noLoop();
-   this.display = function(){
-     strokeWeight(4);
-     stroke(255, 225 , 50);
-     // push()
-     line(this.x, this.y, a, b);
-     push();
-     translate(a, b);
-     line(0, 0, c, d);
-     translate(c, d);
-     line(0, 0, e, f);
-     translate(e, f);
-     line (0, 0, g, h);
-     pop();
-   }
-   
-}
-
-
-
->>>>>>> ricki
